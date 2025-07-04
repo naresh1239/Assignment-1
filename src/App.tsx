16c2438c7ab06fad4dc061655bcbf3cee575cf65
nareshk <nareshk@claptek.com>
2025-07-04 12:36:51 +0530

    update the read me

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
 
   const cardDatafn = async() =>{
     try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/users'); // Replace with your API endpoint
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
       const jdata = await response.json();
       setdata(jdata);
     } catch (error) {
